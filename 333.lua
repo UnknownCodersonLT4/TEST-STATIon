0bec680571ef8d3af33eba850b2ea1863b548791
TheUnknownCoder <coderobierika@gmail.com>
2025-05-30 21:49:35 -0400

    Create 333.lua

---
+++ 333.lua
@@ -0,0 +1,8 @@
+(function()--[[
+local fSBROrciQLVE = {(2-1),(7-5),(8-5),(9-5),(10-5)}[math.random((10-5))]
+  Obfuscated by SLAYERSON
+
+local jDbkCxENs = string.char(math.floor(65.9) + math.random((50/2)))
+  math.floor(2025.9)-(10-5)-31T01:(94-50):(13-5).963Z
+--]]
+print((function() local pikAyDcGM="==gTF1EIFZ1TMBSS"; return pikAyDcGM:reverse():gsub(".", function(c) return string.char(c:byte() - 1) end) end)()) end)()
