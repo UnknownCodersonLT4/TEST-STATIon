093c78624428c8d69bd7867d61cf1edfa388cd9b
TheUnknownCoder <coderobierika@gmail.com>
2025-07-02 03:56:13 -0400

    Update TEStingbad.lua

--- TEStingbad.lua
+++ TEStingbad.lua
@@ -1,32 +1,32 @@
 local old_unpack = unpack
-local old_append = appendfile
+local old_appendfile = appendfile
 
-local output = "yIKESs.txt"
+local output = "yjes.txt"
 
-local Credits = "\n"..[[
--]
-Dumped By Slayerson V1 Dumper bot
--[
+local Credits = "\n" .. [[
+-]\n Dumped By Slayerson V1 Dumper bot \n-[
 ]]
 
 local function appendfile(filename, content)
     if not isfile(filename) then
         writefile(filename, string.format("%s\n%s", Credits, content))
     end
-    return old_append(filename, content)
+    return old_appendfile(filename, content)
 end
 
 local function format(tab, indent)
     indent = indent or 0
-    local result = string.rep(" ", indent) .. "[table]:{\n"
+    local indent_str = string.rep("  ", indent)
+    local result = "[table]:{\n"
     for index, value in pairs(tab) do
+        result = result .. indent_str .. "  [" .. tostring(index) .. "] = "
         if type(value) == "table" then
-            result = result .. string.rep(" ", indent + 2) .. "[" .. tostring(index) .. "] = " .. format(value, indent + 2)
+            result = result .. format(value, indent + 1) .. ",\n"
         else
-            result = result .. string.rep(" ", indent + 2) .. "[" .. tostring(index) .. "] = " .. tostring(value) .. ",\n"
+            result = result .. tostring(value) .. ",\n"
         end
     end
-    result = result .. string.rep(" ", indent) .. "},\n"
+    result = result .. indent_str .. "}"
     return result
 end
 
@@ -34,7 +34,7 @@ local function foreach(tab)
     if type(tab) == "table" then
         for index, value in pairs(tab) do
             if type(value) == "table" then
-                appendfile(output, format(value))
+                appendfile(output, format(value, 1) .. ",\n")
             else
                 appendfile(output, string.format("  [%s] = %s,\n", tostring(index), tostring(value)))
             end
@@ -50,13 +50,5 @@ function unpack(...)
     return result
 end
 
---[[ Script ]]--
-
---> Put your obfuscated script here
---> Using tbo as an example : It will freeze your game for a bit and then create a file named after your output
---> then it will basically just put all the stuff in there, just wait for it to be done
-
---> either loadstring or just the pasted script here
---> btw dont use tbo its skidded lmao
-
+-- Execute the obfuscated script
 loadstring(game:HttpGet("https://raw.githubusercontent.com/cool5013/TBO/refs/heads/main/paid"))()
